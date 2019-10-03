6c58edc126445b687c853f07d064e2dd1cde66ad
cordis-dev <darius@adroiti.com>
2019-10-03 14:45:13 +0300

    Update t.php

--- t.php
+++ t.php
@@ -2,6 +2,11 @@
 
 sql_regcase( 'Foo - bar.'); // Deprecated.
 
+// WordPress.WP.PostsPerPage
+$args = array(
+	'posts_per_page' => 999, // Warning.
+);
+
 final class SomeClass
 {
     public final function SomeMethod()
